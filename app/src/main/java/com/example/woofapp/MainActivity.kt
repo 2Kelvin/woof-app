0aa611228a78e5871c9d5bbdf68e7ba7a526e306
2Kelvin <rocketmancodes02@gmail.com>
2024-05-09 16:32:17 +0300

    add a top app bar

--- app/src/main/java/com/example/woofapp/MainActivity.kt
+++ app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -15,11 +15,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -36,7 +39,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WoofAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize()) {
                     WoofApp()
                 }
@@ -49,7 +51,9 @@ class MainActivity : ComponentActivity() {
 // Composable that displays an app bar and a list of dogs.
 @Composable
 fun WoofApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = { WoofTopAppBar() } // top app bar
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(dogs) {
                 DogItem(
@@ -138,6 +142,35 @@ fun DogInformation(
             style = MaterialTheme.typography.bodyLarge,
         )
     }
+}
+
+/**
+* Composable that creates a custom woof top app bar
+*
+* @param modifier modifiers to style this composable
+*/
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_woof_logo),
+                        contentDescription = null,
+                        modifier = modifier
+                            .size(dimensionResource(id = R.dimen.image_size))
+                            .padding(dimensionResource(id = R.dimen.padding_small))
+                    )
+
+                    Text(
+                        text =  stringResource(id = R.string.woof),
+                        style = MaterialTheme.typography.displayLarge // custom font + style from material theme: Type.kt
+                    )
+                }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview
